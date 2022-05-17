64ebb1f689a2b7cd4ab7d3c73d0359e41ed600c2
luiz gustavo <gustavo.limafgz@gmail.com>
2022-05-16 22:50:37 -0400

    add..

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
-<!-- <?php //Conexão com o Banco de Dados
-$connect = tmysqli_connet("localhost", "root", "");
-$db = mysqli_select_db("$connect", "savepets")
+<?php //Conexão com o Banco de Dados
+
+$host = "localhost";
+$usuario = "id18890523_adminpets";
+$senha = "Im<51VQ3G]SdiFYt";
+$banco = "id18890523_savepets22";
+
+$conexao = new MySQLi ("$host","$usuario","$senha", "$banco" );
+$conexao -> set_charset("utf8");
+
+if ($conexao -> connect_error) {
+
+echo "ERRO DE CONEXÃO";
+
+
+} /* else {
+	 echo "CONEXÃO BEM SUCEDIDA!";
+	} */
+	
+	
+
+
 ?>
-
-
-
-<?php
-$sql = "select * from usuario";
-$query = mysqli_query($db, $sql) or die ("Erro na query do banco de dados=" .mysqli_error($db));
-while($resultado = mysqli_fetch_assoc($query)){
-
-echo $resultado['usunome'];
-?> -->
